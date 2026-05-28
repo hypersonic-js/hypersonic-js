f1f1eedc13ad4d7955e2f5b169a6cfc61dd42d25
zesuperaker <bobafettskywalker@gmail.com>
2026-05-27 23:12:16 -0700

    update test app 1.7

--- apps/test-app/vite.config.ts
+++ apps/test-app/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  build: {
+    outDir: 'public',
+    manifest: true,
+    rollupOptions: {
+      input: 'resources/js/app.tsx',
+    },
+  },
   test: {
     environment: 'node',
     include: ['tests/**/*.test.ts'],
